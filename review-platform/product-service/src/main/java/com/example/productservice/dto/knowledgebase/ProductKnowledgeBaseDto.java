09d09a583685d9d3cc9ced09b3055fe9006f887d
Phuong Anh Nguyen <alixblu3011@gmail.com>
2025-11-28 22:36:16 +0700

    wip: upload data to s3 knowledge base

---
+++ review-platform/product-service/src/main/java/com/example/productservice/dto/knowledgebase/ProductKnowledgeBaseDto.java
@@ -0,0 +1,29 @@
+package com.example.productservice.dto.knowledgebase;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+public record ProductKnowledgeBaseDto(
+        String id,
+        String name,
+        BrandInfo brand,
+        String category,
+        List<String> ingredients,
+        String description,
+        @JsonProperty("concern_type") List<String> concernType,
+        @JsonProperty("skin_type") List<String> skinType,
+        long price,
+        float rating,
+        String status,
+        @JsonProperty("updated_at") LocalDateTime updatedAt
+) {
+    @Builder
+    public record BrandInfo(
+            String id,
+            String name
+    ) {}
+}
